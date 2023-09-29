878886e20eeac34586a4df789f8902e1229e7297
LordDemecrius83 <lorddemecrius83@proton.me>
2023-09-29 21:09:34 +0800

    Fix formatting

--- rw-system.sh
+++ rw-system.sh
@@ -1163,5 +1163,5 @@ fi
 # Fixes Vibrator on TECNO POVA 4 Pro
 
 if getprop ro.product.vendor.device | grep -q -e TECNO-LG8n; then
-  chown -R system:system /sys/class/leds/vibrator_single/
+    chown -R system:system /sys/class/leds/vibrator_single/
 fi

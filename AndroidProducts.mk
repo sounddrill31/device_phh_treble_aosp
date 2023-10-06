c031fb0fc9b671d6ea7fdd3d18e34a11ec929d46
SoundDrill31 <84176052+sounddrill31@users.noreply.github.com>
2023-10-06 18:23:34 +0530

    add arrow_gsi

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,2 +1,5 @@
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/aosp_arm64_bgN.mk \
+    $(LOCAL_DIR)/arrow_gsi.mk \
+COMMON_LUNCH_CHOICES := \
+    arrow_gsi-userdebug \
---
+++ arrow_gsi.mk
@@ -0,0 +1,36 @@
+$(call inherit-product, device/phh/treble/base-pre.mk)
+include build/make/target/product/aosp_arm64.mk
+$(call inherit-product, device/phh/treble/base.mk)
+
+# Use Scudo instead of Jemalloc
+# PRODUCT_USE_SCUDO := true
+
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_GAPPS_ARCH := arm64
+
+PRODUCT_NAME := aosp_arm64_bgN
+PRODUCT_DEVICE := arm64_bgN
+PRODUCT_BRAND := google
+PRODUCT_SYSTEM_BRAND := google
+PRODUCT_MODEL := Phh-Treble GSI
+
+# Disable "emulator" characteristics
+PRODUCT_CHARACTERISTICS := device
+
+# Project-Elixir
+PRODUCT_PACKAGES += OverlaySettings
+IS_PHONE := true
+TARGET_SUPPORTS_QUICK_TAP := true
+TARGET_SUPPORTS_GOOGLE_RECORDER := false
+TARGET_INCLUDE_STOCK_ACORE := false
+TARGET_INCLUDE_LIVE_WALLPAPERS := false
+TARGET_FACE_UNLOCK_SUPPORTED := true
+ELIXIR_BUILD_TYPE := OFFICIAL
+TARGET_BUILD_APERTURE_CAMERA := true
+
+# GMS
+ifeq ($(WITH_GMS),true)
+GMS_MAKEFILE=gms_minimal.mk
+endif

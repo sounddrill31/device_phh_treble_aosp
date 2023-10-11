57a814d66edc0796afe05f577b6a3dadb5fd5188
ItsLynix <71310187+ItsLynix@users.noreply.github.com>
2023-10-12 00:20:17 +0200

    vendorsetup: fixup

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -52,6 +52,7 @@ rm -rf prebuilts/vndk/v28
 git clone https://android.googlesource.com/platform/prebuilts/vndk/v28 ./prebuilts/vndk/v28
 cd prebuilts/vndk/v28
 git reset --hard 204f1bad00aaf480ba33233f7b8c2ddaa03155dd
+cd ../../..
 
 apply "TrebleDroid"
 apply "UniversalX"
